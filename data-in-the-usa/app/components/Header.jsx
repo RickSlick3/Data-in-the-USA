642009575757f077ff561664bd08375afa5ae99b
rickslick3 <richard.robertsf@gmail.com>
2025-02-23 12:29:07 -0500

    basic setup

---
+++ data-in-the-usa/app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as d3 from 'd3';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Header
--- data-in-the-usa/tailwind.config.mjs
+++ data-in-the-usa/tailwind.config.mjs
@@ -8,10 +8,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#aeb6f1',
+        darkHover: '#8193e9',
+        'orange-0': '#fcb34d',
+        'orange-1': '#f9aa33',
+        'orange-2': '#ffbd65',
+        'gray-0': '#4a6572',
+        'gray-1': '#344955',
+        'gray-2': '#232f34',
       },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      boxShadow: {
+        'black': '3px 3px 0 #000', 
+        'white': '3px 3px 0 #fff',
+      }
     },
+    gridTemplateColumns: {
+      'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
+    }
   },
   plugins: [],
 };
