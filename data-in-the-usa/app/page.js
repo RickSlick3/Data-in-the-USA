273bd630b28ae6389ceb7bda3b0006ab0aadbb2a
rickslick3 <richard.robertsf@gmail.com>
2025-02-23 21:50:26 -0500

    sidebar and mobile header

--- data-in-the-usa/app/page.js
+++ data-in-the-usa/app/page.js
@@ -1,10 +1,12 @@
 'use client'
-import Header from "./components/Header";
+import Layout from "./components/Layout";
 
 export default function Home() {
   return (
     <>
-    <Header />
+    <Layout>
+      
+    </Layout>
     </>
   );
 }
