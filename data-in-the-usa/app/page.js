a6023367bd6a4ceae6ae45347bdc20cf19f34c4f
rickslick3 <richard.robertsf@gmail.com>
2025-02-26 20:52:14 -0500

    home page documentation reference

--- data-in-the-usa/app/page.js
+++ data-in-the-usa/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Is Health Really Wealth? </h1>
-      <p>This is the Documentation page. Put your docs here.</p>
+      <p>Documentation will be found <a className='underline text-gray-500' href='https://github.com/RickSlick3/Data-in-the-USA' target="_blank">here</a> for now.</p>
     </div>
   );
 }
